bc9921035753d51ce676f7536bf3677a0d86aea7
João <joaopedrodealmeidaleme329@gmail.com>
2025-04-30 20:20:51 -0300

    ex22

---
+++ ex22/ex22.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    float numero;
+
+    printf("Seu numero: ");
+    scanf("%f",&numero);
+
+    if (numero>10)
+    {
+        printf("Seu numero e maior que 10");
+    }
+    if (numero<10)
+    {
+        printf("Seu numero e menor que 10");
+    }
+    
+}
