1962ded6189fd95f14fde592bc4544efb4751718
João <joaopedrodealmeidaleme329@gmail.com>
2025-05-02 23:13:52 -0300

    ex31

--- ex31/ex31.c
+++ ex31/ex31.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main (){
+    float altura, pesoideal;
+    char  nome, sexo;
+
+    printf("Digite seu nome: ");
+    scanf("%s",&nome);
+
+    printf("Qual o seu sexo?(M ou F): ");
+    scanf("%s",&sexo);
+
+    printf("Qual a sua altura?(Em metros): "); 
+    scanf("%f",&altura);
+
+    if (sexo == 'M')
+    {
+        pesoideal = (72.7 * altura) - 58;
+        printf ("Seu peso ideal é %2.f", pesoideal);
+    }
+
+if (sexo == 'F')
+    {
+        pesoideal = (62.1 * altura) - 44.7;
+        printf ("Seu peso ideal é %2.f", pesoideal);
+    }
+    
     
 
 
